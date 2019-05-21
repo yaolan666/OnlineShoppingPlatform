17f0abcd084162db627d6e8d86512a2164edf4ff
yaolan666 <Y13624516233@163.com>
2019-05-21 09:18:25 +0800

    20190521新增加的类更新一下

---
+++ src/main/java/com/youzhi/controller/RedisController.java
@@ -0,0 +1,15 @@
+package com.youzhi.controller;
+
+import com.jfinal.core.Controller;
+import com.jfinal.plugin.redis.Cache;
+import com.jfinal.plugin.redis.Redis;
+import redis.clients.jedis.Jedis;
+
+public class RedisController extends Controller {
+    public void test() {
+        Jedis userCache = Redis.use("onlineShopPlatform").getJedis();
+        userCache.set("qwery", "qwery");
+        renderText(userCache.get("qwery"));
+    }
+
+}
---
+++ src/main/java/com/youzhi/model/RedisGenerator.java
@@ -0,0 +1,90 @@
+package com.youzhi.model;
+ 
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+ 
+public final class RedisGenerator {
+ 
+    //Redis服务器IP
+    private static String ADDR = "127.0.0.1";
+ 
+    //Redis的端口号
+    private static int PORT = 6379;
+ 
+    //访问密码
+    private static String AUTH = "";
+ 
+    //可用连接实例的最大数目，默认值为8；
+    //如果赋值为-1，则表示不限制；如果pool已经分配了maxActive个jedis实例，则此时pool的状态为exhausted(耗尽)。
+    private static int MAX_ACTIVE = -1;
+ 
+    //控制一个pool最多有多少个状态为idle(空闲的)的jedis实例，默认值也是8。
+    private static int MAX_IDLE = 200;
+ 
+    //等待可用连接的最大时间，单位毫秒，默认值为-1，表示永不超时。如果超过等待时间，则直接抛出JedisConnectionException；
+    private static int MAX_WAIT = -1;
+ 
+    private static int TIMEOUT = 10000;
+ 
+    //在borrow一个jedis实例时，是否提前进行validate操作；如果为true，则得到的jedis实例均是可用的；
+    private static boolean TEST_ON_BORROW = true;
+ 
+    private static JedisPool jedisPool = null;
+ 
+    /**
+     * 初始化Redis连接池
+     */
+    static {
+        try {
+            JedisPoolConfig config = new JedisPoolConfig();
+//            config.setMaxTotal(MAX_ACTIVE);
+//            config.setMaxIdle(MAX_IDLE);
+//            config.setMaxWaitMillis(MAX_WAIT);
+//            config.setTestOnBorrow(TEST_ON_BORROW);
+            config.setMaxTotal(32);//最大连接数
+            config.setMaxIdle(200);//闲置最大连接数
+            config.setMinIdle(10);//闲置最小连接数
+            config.setMaxWaitMillis(25000);//到达最大连接数后，调用者阻塞时间
+            config.setMinEvictableIdleTimeMillis(300000);//连接空闲的最小时间，可能被移除
+            config.setSoftMinEvictableIdleTimeMillis(-1);//连接空闲的最小时间，多余最小闲置连接的将被移除
+            config.setNumTestsPerEvictionRun(3);//设置每次检查闲置的个数
+            config.setTestOnBorrow(false);//申请连接时，是否检查连接有效
+            config.setTestOnReturn(false);//返回连接时，是否检查连接有效
+            config.setTestWhileIdle(false);//空闲超时,是否执行检查有效
+            config.setTimeBetweenEvictionRunsMillis(60000);//空闲检查时间
+            config.setBlockWhenExhausted(true);//当连接数耗尽，是否阻塞
+            jedisPool = new JedisPool(config, ADDR, PORT, TIMEOUT);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+ 
+    /**
+     * 获取Jedis实例
+     * @return
+     */
+    public static Jedis getJedis() {
+        try {
+            if (jedisPool != null) {
+                Jedis resource = jedisPool.getResource();
+                return resource;
+            } else {
+                return null;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+ 
+    /**
+     * 释放jedis资源
+     * @param jedis
+     */
+    public static void returnResource(final Jedis jedis) {
+        if (jedis != null) {
+            jedisPool.returnResource(jedis);
+        }
+    }
+}
